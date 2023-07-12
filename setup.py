7831706b5189ef7aa08864b6cecd999a2179de2c
Alexander Young <alexander.young@pingthings.io>
2023-07-12 17:15:22 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup
 
 setup(
     name='jupyterhub-jwtauthenticator-v2',
-    version='2.0.3',
+    version='2.1.0',
     description='JSONWebToken Authenticator for JupyterHub',
-    url='https://github.com/izihawa/jwtauthenticator_v2',
-    author='ppodolsky',
-    author_email='ppodolsky@me.com',
+    url='https://github.com/PingThingsIO/jwtauthenticator_v2',
+    author='youngale-pingthings',
+    author_email='alexander.young@pingthings.io',
     license='Apache 2.0',
     packages=['jwtauthenticator'],
     install_requires=[
